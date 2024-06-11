94574ad3b03b6c814198f99793cc0f4d5adfeb48
Lee Junhee <junhee0110@gmail.com>
2024-06-11 19:29:58 +0900

    [feat] ADD z-directional move
    - 높이 조절도 가능하도록 수정
    - 이제 비행 전략을 구현할 때, xy 움직임과 z 움직임을
      정의해주면 됨

--- obstacle_avoidance/automatic_flight_controller/direction_controller.py
+++ obstacle_avoidance/automatic_flight_controller/direction_controller.py
@@ -7,62 +7,51 @@ from automatic_flight_controller.drone_data import DroneData
 
 
 class DirectionController:
-    def __init__(self, node: Node, config_dict: dict, data: DroneData):
-        self.__node = node
-        self.__config : dict = config_dict
-        self.__model_ns = self.__config.get("namespace", "simple_drone")
+    def __init__(self, config_dict: dict, data: DroneData):
+        self._config : dict = config_dict
         self.__data = data
         self.__target_direction : float = 0.0
-        
-        # Publisher
-        self.control_publisher = self.__node.create_publisher(Twist, "/{}/cmd_vel".format(self.__model_ns), 10)
+        self.__speed  = 0.0
 
-    def flight_control(self, target_direction : float):
+    def flight_control(self, target_direction : float, speed : float):
         self.__target_direction = target_direction
+        self.__speed = speed
 
-        if self.__is_arrival():
-            self.__publish_stop()
-        else:
-            self.__publish_control()
+        return self.__calculate_control()
 
+        
+    def __calculate_control(self):
+        if not(self.__is_arrival()):
+            k_p = self._config.get("directionProportionalGain", 2.0)
+
+            direction_error = calculate_direction_err(self.__target_direction, self.__data.heading_direction)
+
+            angular_vel = Vector3(
+                x = 0.0,
+                y = 0.0,
+                z = (k_p * direction_error)
+            )
+
+            linear_vel = Vector3(
+                x = self.__speed * math.cos(direction_error),
+                y = self.__speed * math.sin(direction_error),
+                z = 0.0
+            )
+            
+            return Twist(linear = linear_vel,
+                        angular = angular_vel)
+        
+        else: 
+            return Twist()
+    
+    
     def __is_arrival(self):
-        arrival_range = self.__config.get("arrivalRange", 1)
+        arrival_range = self._config.get("arrivalRange", 1)
 
         distance = calculate_distance(self.__data.target, self.__data.gps)
-        self.__node.get_logger().info("distance to target : %f" % distance)
 
         return distance < arrival_range
-        
-    def __publish_control(self):
-        k_p = self.__config.get("directionProportionalGain", 2.0)
-        speed = self.__config.get("speed", 10)
 
-        direction_error = calculate_direction_err(self.__target_direction, self.__data.heading_direction)
-        self.__node.get_logger().info("err : %f" % direction_error)
-
-        angular_vel = Vector3(
-            x = 0.0,
-            y = 0.0,
-            z = (k_p * direction_error)
-        )
-
-        linear_vel = Vector3(
-            x = speed * math.cos(direction_error),
-            y = speed * math.sin(direction_error),
-            z = 0.0
-        )
-        
-        self.control_publisher.publish(
-            Twist(linear = linear_vel,
-                  angular = angular_vel)
-        )
-
-    def __publish_stop(self):
-        self.__data.target_received = False
-        self.control_publisher.publish(
-            Twist(linear = Vector3(x = 0.0, y = 0.0, z = 0.0),
-                  angular = Vector3(x = 0.0, y = 0.0, z = 0.0))
-        )
 
 def calculate_direction_err(target, current):
     err = target - current
--- obstacle_avoidance/automatic_flight_controller/fly_strategy.py
+++ obstacle_avoidance/automatic_flight_controller/fly_strategy.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 from automatic_flight_controller.direction_controller import DirectionController
 from automatic_flight_controller.drone_data import DroneData
 from std_msgs.msg import Float32
-
+from geometry_msgs.msg import Twist
 from abc import abstractmethod
 
 class FlyStrategy(Node):
@@ -15,23 +15,42 @@ class FlyStrategy(Node):
         self.__config_dict : dict = get_config(drone_name)
         self.__model_ns = self.__config_dict.get("namespace")
         self.drone_data = DroneData(self, self.__config_dict)
-        self.__controller = DirectionController(self, self.__config_dict, self.drone_data)
+        self.__direction_controller = DirectionController(self.__config_dict, self.drone_data)
+        self.__control_twist = Twist()
         
+        # Publisher
+        self.__control_publisher = self.create_publisher(Twist, "/{}/cmd_vel".format(self.__model_ns), 10)
         self.__target_direction_publish = self.create_publisher(Float32, "/{}/target_direction".format(self.__model_ns), 10)
 
         self.__timer = self.create_timer(0.001, self.__fly)
 
     def __fly(self):
         if (self.drone_data.target_received): 
-            target_direction = self.calculate_direction()
+            speed, target_direction = self.calculate_xy_velocity()
             self.__publish_target_direction(target_direction)
-            self.__controller.flight_control(target_direction)
+            self.__calculate_xy_control(target_direction, speed)
+            self.__calculate_z_control()
+            self.__publish_control()
+            
 
     def __publish_target_direction(self, target_direction):
         self.__target_direction_publish.publish(Float32(data = target_direction))
 
+    def __calculate_xy_control(self, target_direction, speed):
+        self.__control_twist = self.__direction_controller.flight_control(target_direction, speed)
+
+    def __calculate_z_control(self):
+        self.__control_twist.linear.z = self.calculate_z_velocity()
+
+    def __publish_control(self):
+        self.__control_publisher.publish(self.__control_twist)
+
     @abstractmethod
-    def calculate_direction(self):
+    def calculate_xy_velocity(self):
+        return (0.0, 0.0)
+    
+    @abstractmethod
+    def calculate_z_velocity(self):
         return 0.0
 
 

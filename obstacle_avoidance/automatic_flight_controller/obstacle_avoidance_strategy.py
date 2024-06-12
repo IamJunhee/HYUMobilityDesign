7813e9ad4f5fa2d6a2b1a3ec3aeff148d2ea68a6
mobility <junhee0110@hanyang.ac.kr>
2024-06-12 19:26:31 +0900

    [refac] 코드 정리

--- obstacle_avoidance/automatic_flight_controller/obstacle_avoidance_strategy.py
+++ obstacle_avoidance/automatic_flight_controller/obstacle_avoidance_strategy.py
@@ -11,8 +11,26 @@ class ObstacleAvoidanceFly(FlyStrategy):
 
     def calculate_xy_velocity(self):
         range_arr = np_replace_inf_to(np.array(self.drone_data.lidar.ranges), self.drone_data.lidar.range_max)
-        obstacle_prob_arr = np.log(range_arr / 2)
+        no_obstacle_prob_arr = np.log(range_arr / 2)
 
+        target_direction = self.__calculate_target_angle()
+        angle_array = self.__generate_angle_array()
+
+        angle_diff_array = angle_array - target_direction 
+        target_prob_arr = np.cos(angle_diff_array)
+
+        prob_product_arr = no_obstacle_prob_arr * target_prob_arr
+        prob_product_arr = sum_around(prob_product_arr, 100)
+        
+        max_index = np.argmax(prob_product_arr)
+        expected_val = angle_array[max_index]
+
+        self.get_logger().info("Target Direction : %f" % target_direction)
+        self.get_logger().info("expected Direction : %f" % expected_val)
+
+        return 30, expected_val
+    
+    def __calculate_target_angle(self):
         direction_arr = np.array((
                 -(self.drone_data.target.longitude - self.drone_data.gps.longitude),
                 -(self.drone_data.target.latitude - self.drone_data.gps.latitude)
@@ -20,27 +38,18 @@ class ObstacleAvoidanceFly(FlyStrategy):
 
         direction_arr = direction_arr / np.linalg.norm(direction_arr, 2)
 
-        temp_target_direction = math.atan2(direction_arr[1], direction_arr[0])
-        target_direction = (temp_target_direction + 2 * math.pi) \
-                                    if temp_target_direction < 0 \
-                                    else temp_target_direction
-
+        target_direction = math.atan2(direction_arr[1], direction_arr[0])
+        return (target_direction + 2 * math.pi) \
+                                    if target_direction < 0 \
+                                    else target_direction
+    
+    def __generate_angle_array(self):
         angle_array = np.linspace(self.drone_data.lidar.angle_min, self.drone_data.lidar.angle_max, len(self.drone_data.lidar.ranges))
         angle_array += self.drone_data.heading_direction
         np.putmask(angle_array, angle_array < 0, angle_array + 2 * np.pi)
-        angle_array2 = angle_array - target_direction 
-        angle_array2 = np.cos(angle_array2)
 
-        obs_prob_arr2 = obstacle_prob_arr * angle_array2
-        obs_prob_arr2 = sum_around(obs_prob_arr2, 100)
-        max_index = np.argmax(obs_prob_arr2)
-        expected_val = angle_array[max_index]
+        return angle_array
 
-        self.get_logger().info("Target Direction : %f" % target_direction)
-        self.get_logger().info("expected Direction : %f" % expected_val)
-
-        return 30, expected_val
-        
     def calculate_z_velocity(self):
         err = self.drone_data.target.altitude - self.drone_data.gps.altitude + 10
         return err
